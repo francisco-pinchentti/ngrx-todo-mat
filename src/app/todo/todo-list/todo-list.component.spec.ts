9bc72ca9b013663e192c84c50f68fcca11d0ab32
Francisco Pinchentti <f.pinchentti@globant.com>
2020-04-21 17:14:08 -0300

    updated

--- src/app/todo/todo-list/todo-list.component.spec.ts
+++ src/app/todo/todo-list/todo-list.component.spec.ts
@@ -1,15 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import { TodoListComponent } from './todo-list.component';
+
+const initialState = {
+    todos: [],
+    selectedTodo: null,
+};
+
+// mock a child component:
+@Component({
+    selector: 'app-todo-summary-item',
+    template: '<div></div>',
+})
+class MockedTodoSummaryItemComponent {}
 
 describe('TodoListComponent', () => {
     let component: TodoListComponent;
     let fixture: ComponentFixture<TodoListComponent>;
+    let store: MockStore;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [TodoListComponent],
+            declarations: [TodoListComponent, MockedTodoSummaryItemComponent],
+            imports: [NoopAnimationsModule, MatExpansionModule, MatDividerModule, MatBadgeModule],
+            providers: [provideMockStore({ initialState })],
         }).compileComponents();
+        store = TestBed.inject(MockStore);
     }));
 
     beforeEach(() => {
--- src/app/todo/todo-summary-item/todo-summary-item.component.spec.ts
+++ src/app/todo/todo-summary-item/todo-summary-item.component.spec.ts
@@ -1,15 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+// ngrx
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
+// material
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+// component
 import { TodoSummaryItemComponent } from './todo-summary-item.component';
+
+// mocks store
+const initialState = {
+    todos: [],
+    selectedTodo: null,
+};
 
 describe('TodoSummaryItemComponent', () => {
     let component: TodoSummaryItemComponent;
     let fixture: ComponentFixture<TodoSummaryItemComponent>;
+    let store: MockStore;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [TodoSummaryItemComponent],
+            imports: [
+                NoopAnimationsModule,
+                ReactiveFormsModule,
+                MatButtonModule,
+                MatFormFieldModule,
+                MatInputModule,
+                MatCheckboxModule,
+            ],
+            providers: [provideMockStore({ initialState })],
         }).compileComponents();
+
+        store = TestBed.inject(MockStore);
     }));
 
     beforeEach(() => {
