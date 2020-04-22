a9a05143e03099a48b3a2d6f1955d6f7559995ca
Francisco Pinchentti <francisco.pinchentti@gmail.com>
2020-04-22 12:45:17 -0300

    ut for form

--- src/app/todo/todo-summary-item/todo-summary-item.component.spec.ts
+++ src/app/todo/todo-summary-item/todo-summary-item.component.spec.ts
@@ -20,10 +20,39 @@ const initialState = {
     selectedTodo: null,
 };
 
+class TodoSummaryItemComponentPage {
+    component: TodoSummaryItemComponent;
+
+    constructor(private fixture: ComponentFixture<TodoSummaryItemComponent>) {
+        this.component = fixture.componentInstance;
+    }
+
+    getHTMLElement<T>(selector: string): T {
+        return <T>this.fixture.nativeElement.querySelector(selector);
+    }
+
+    get titleFieldHTMLElement(): HTMLInputElement {
+        return this.getHTMLElement<HTMLInputElement>('input[formControlName="title"]');
+    }
+
+    get matCheckBoxHTMLElement(): HTMLElement {
+        return this.getHTMLElement<HTMLElement>('mat-checkbox .mat-checkbox-input');
+    }
+
+    get bodyFieldHTMLElement(): HTMLTextAreaElement {
+        return this.getHTMLElement<HTMLTextAreaElement>('textarea[formControlName="body"]');
+    }
+
+    get submitButton(): HTMLButtonElement {
+        return this.getHTMLElement<HTMLButtonElement>('button[color="accent"]');
+    }
+}
+
 describe('TodoSummaryItemComponent', () => {
     let component: TodoSummaryItemComponent;
     let fixture: ComponentFixture<TodoSummaryItemComponent>;
     let store: MockStore;
+    let page: TodoSummaryItemComponentPage;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -44,11 +73,45 @@ describe('TodoSummaryItemComponent', () => {
 
     beforeEach(() => {
         fixture = TestBed.createComponent(TodoSummaryItemComponent);
+        page = new TodoSummaryItemComponentPage(fixture);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should have a disabled submit button', () => {
+        expect(page.submitButton.getAttribute('disabled')).toBe('true');
+    });
+
+    describe('when mandatory fields are correctly filled', () => {
+        it('should enable the submit button', async(() => {
+            page.titleFieldHTMLElement.value = 'a title';
+            page.titleFieldHTMLElement.dispatchEvent(new Event('input'));
+            page.bodyFieldHTMLElement.value = 'a brief body';
+            page.bodyFieldHTMLElement.dispatchEvent(new Event('input'));
+            page.matCheckBoxHTMLElement.click();
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+                expect(component.form.valid).toBeTrue();
+                expect(page.submitButton.getAttribute('disabled')).toBeFalsy();
+            });
+        }));
+    });
+
+    describe('when mandatory fields are incorrectly filled', () => {
+        it('should disable the submit button', async(() => {
+            page.titleFieldHTMLElement.value = null;
+            page.titleFieldHTMLElement.dispatchEvent(new Event('input'));
+            page.bodyFieldHTMLElement.value = 'a'.repeat(500);
+            page.bodyFieldHTMLElement.dispatchEvent(new Event('input'));
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+                expect(component.form.invalid).toBeTrue();
+                expect(page.submitButton.getAttribute('disabled')).toBe('true');
+            });
+        }));
     });
 });
